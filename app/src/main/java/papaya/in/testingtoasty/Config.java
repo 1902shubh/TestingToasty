d86ff63f88dd598441deb5632d99139f7109c2ad
Shubham Pandey <shubhampandey979271@gmail.com>
2021-07-05 08:36:33 +0530

    Initial commit

---
+++ app/src/main/java/papaya/in/testingtoasty/Config.java
@@ -0,0 +1,6 @@
+package papaya.in.testingtoasty;
+
+public class Config {
+    public static final String EMAIL ="progressify.info@gmail.com";
+    public static final String PASSWORD ="$progr#ESS@$@ify$";
+}
---
+++ app/src/main/java/papaya/in/testingtoasty/MainActivity.java
@@ -0,0 +1,27 @@
+package papaya.in.testingtoasty;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import papaya.in.sendmail.SendMail;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SendMail mail = new SendMail(Config.EMAIL, Config.PASSWORD,
+                "shubhampandey979271@gmail.com",
+                "Testing my library",
+                "It's working.\n Subscribe Papaya Coders for more amazing videos");
+
+
+        findViewById(R.id.mail).setOnClickListener(v ->
+                mail.execute()
+                );
+
+
+    }
+}
