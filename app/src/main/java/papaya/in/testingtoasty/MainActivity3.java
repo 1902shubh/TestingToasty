630ad3ba0a9a5e93552f0e11a2e2d548e991166c
Shubham Pandey <shubhampandey979271@gmail.com>
2021-07-05 08:49:08 +0530

    remove permission

---
+++ app/src/main/java/papaya/in/testingtoasty/MainActivity3.java
@@ -0,0 +1,14 @@
+package papaya.in.testingtoasty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+}
